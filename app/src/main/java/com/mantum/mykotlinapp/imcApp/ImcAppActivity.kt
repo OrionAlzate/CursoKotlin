3927372938409955d556232e7ab8e61ac1e3b68d
OrionAlzate <orion.alzate@Mantum.com.co>
2023-10-05 12:42:07 -0500

    Componentes edad y peso listos con logica

--- app/src/main/java/com/mantum/mykotlinapp/imcApp/ImcAppActivity.kt
+++ app/src/main/java/com/mantum/mykotlinapp/imcApp/ImcAppActivity.kt
@@ -3,19 +3,37 @@ package com.mantum.mykotlinapp.imcApp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
 import com.mantum.mykotlinapp.MenuActivity
 import com.mantum.mykotlinapp.R
+import java.text.DecimalFormat
 
 class ImcAppActivity : AppCompatActivity() {
 
     private var isMaleSelected : Boolean = true
     private var isFemaleSelected : Boolean = false
+    private var pesoInicial = 60
+    private var edadInicial = 22
 
     private  lateinit var viewMale:CardView
     private lateinit var viewFemale:CardView
+    private lateinit var tv_laAltura : TextView
+    private lateinit var rsAltura : RangeSlider
+
+    private lateinit var tvPeso : TextView
+    private lateinit var tvEdad : TextView
+
+    private lateinit var btnMenosPeso : FloatingActionButton
+    private lateinit var btnMasPeso : FloatingActionButton
+    private lateinit var btnMenosEdad : FloatingActionButton
+    private lateinit var btnMasEdad : FloatingActionButton
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,11 +51,23 @@ class ImcAppActivity : AppCompatActivity() {
             initComponents()
             initListeners()
             initUI()
+
+
     }
 
     private fun initComponents(){
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+        tv_laAltura = findViewById(R.id.laAltura)
+        rsAltura = findViewById(R.id.rsAltura)
+        btnMenosPeso = findViewById(R.id.btnMenosPeso)
+        btnMasPeso = findViewById(R.id.btnMasPeso)
+        tvPeso = findViewById(R.id.tvPeso)
+        tvEdad = findViewById(R.id.tvEdad)
+        btnMenosEdad = findViewById(R.id.btnMenosEdad)
+        btnMasEdad = findViewById(R.id.btnMasEdad)
+
+
     }
     private fun initListeners() {
         viewMale.setOnClickListener {
@@ -49,6 +79,30 @@ class ImcAppActivity : AppCompatActivity() {
             changeGender()
             setGenderColor()
         }
+        rsAltura.addOnChangeListener { _, value, _ ->
+            val decimalFormat = DecimalFormat("#.##")
+            val result = decimalFormat.format(value)
+            tv_laAltura.text = "$result cm"
+        }
+        btnMenosPeso.setOnClickListener(View.OnClickListener {
+            pesoInicial--
+            actualizarPeso()
+
+        })
+        btnMasPeso.setOnClickListener(View.OnClickListener {
+            pesoInicial++
+            actualizarPeso()
+        })
+
+        btnMenosEdad.setOnClickListener(View.OnClickListener {
+            edadInicial--
+            actualizarEdad()
+        })
+        btnMasEdad.setOnClickListener(View.OnClickListener {
+            edadInicial++
+            actualizarEdad()
+        })
+
     }
 
     private fun changeGender(){
@@ -74,6 +128,16 @@ class ImcAppActivity : AppCompatActivity() {
 
     private fun initUI(){
         setGenderColor()
+        actualizarPeso()
+        actualizarEdad()
+    }
+
+    private fun actualizarPeso(){
+        tvPeso.setText("$pesoInicial kg")
+    }
+
+    private fun actualizarEdad(){
+        tvEdad.setText("$edadInicial años")
     }
 
 
